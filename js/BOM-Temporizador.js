a0eea7c961b4e3206ca854a73e7656e3bf71e4be
abelcg <abelcg86@gmail.com>
2021-10-03 08:14:34 -0300

    BOM Cronometro

---
+++ js/BOM-Temporizador.js
@@ -0,0 +1,7 @@
+/* Temporizador
+
+Realizar una web con un temporizador donde el usuario pueda ingresar un tiempo desde donde comenzará a 
+decrementar el contador. Debe contener los botones, iniciar, pausar y reset. 
+ */
+
+ 
